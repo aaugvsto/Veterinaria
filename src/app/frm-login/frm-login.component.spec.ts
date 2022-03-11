f1e0c8ce1c455b5f316fdd153540e9ef193f612b
Augusto Assis <11801964@aluno.cotemig.com.br>
2022-03-11 10:12:03 -0300

    first commit

---
+++ src/app/frm-login/frm-login.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FrmLoginComponent } from './frm-login.component';
+
+describe('FrmLoginComponent', () => {
+  let component: FrmLoginComponent;
+  let fixture: ComponentFixture<FrmLoginComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FrmLoginComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FrmLoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/frm-login/frm-login.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-frm-login',
+  templateUrl: './frm-login.component.html',
+  styleUrls: ['./frm-login.component.css']
+})
+export class FrmLoginComponent {
+
+  constructor() { }
+
+}
